bc67cc7fe7e752c96528b6591b95ab1a81067249
OtavioSessaSouza <sessa.otavio@gmail.com>
2019-06-06 16:24:14 -0400

    gera_vi atualizado/teste do gerarelatorio/ lista de vi atualizado/ experiment.json atualizado

--- app/gera_vi.js
+++ app/gera_vi.js
@@ -1,49 +1,95 @@
-
-var vi = [];
-var interval=0;
+var interval_componentA=0;
+var vi_componentA = [];
+var index_phase=0;
+var number_of_phases_componentA;
+var interval_componentB=0;
+var vi_componentB = [];
+var index_phase=0;
+var number_of_phases_componentB;
 module.exports = {
-  gera_vi: function(){//está função devolve um valor do vi especifico
-    //console.log("vi antes:",vi);
+  gera_vi_componentA: function(){//está função devolve um valor do vi especifico
 
-
-    if(vi[0]===undefined){
-      vi=module.exports.load_lista();//isso talvez não deve ficar em definitivo aqui
-      vi=module.exports.embaralha(vi);//embaralha aqui
-      console.log("vi carregado:",vi);
+    if(vi_componentA[0]===undefined){
+      vi_componentA=module.exports.carries_lista_componentA();//isso talvez não deve ficar em definitivo aqui
+      vi_componentA=module.exports.embaralha(vi_componentA);//embaralha aqui
     }
-
-    interval=module.exports.gera_intervalo(vi,interval);
-    vi=module.exports.remove_da_lista(vi,interval);
-    console.log("vi:",vi);
-    console.log("interval:",interval);
-    return interval;
+    interval_componentA=module.exports.gera_intervalo(vi_componentA,interval_componentA);//talvez tenha problema
+    vi_componentA=module.exports.remove_da_lista(vi_componentA,interval_componentA);
+    return interval_componentA;
   },
   gera_intervalo: function(vi){//está função devolve um valor do vi especifico
-    //console.log("vi antes:",vi);
     var index=Math.floor((Math.random() * vi.length));
     var returns= vi[index];
     return returns;
   },
-  load_lista : function(){//carrega o json para as variaveis
-      var archive;
-      var fs = require('fs'),
-      path = require('path')
-      filePath = path.join(__dirname+'/json', 'lista.json' );
-      //console.log(__dirname);
-      try {
-        var fsarchive = fs.readFileSync(filePath)
-        var archive = JSON.parse(fsarchive);
-      } catch (error) {
+  carries_lista_componentA : function(){//carrega o json para as variaveis
+    var lista=module.exports.return_lista();
+    var vi_componentA=module.exports.define_vi_cpA(index_phase);
+    var vi_used=module.exports.load_vi_componentA(lista)//vi_used//_componentA
+    var max=module.exports.max_of_vi(vi_used,lista)
+    return module.exports.interval_list(lista,vi_used);
+  },
+  interval_list:function(lista,vi_used){
+    var list=lista
+    var vi_usado=['VI.vi',vi_used,'.vi']
+    vi_usado=vi_usado.join('')
+    vi_usado = vi_usado.split('.')
+    vi_usado.forEach((item) => {
+      list = list[item]
+    })
+    return list
+  },
+  max_of_vi:function(vi_used,lista){
+    var max_of_vi=lista
+    var vi_usado=['VI.vi',vi_used,'.tamanho']
+    vi_usado=vi_usado.join('')
+    vi_usado = vi_usado.split('.')
+    vi_usado.forEach((item) => {
+      max_of_vi = max_of_vi[item]
+    })
+    return max_of_vi
+
+  },
+  load_vi_componentA: function(lista){//func em teste
+    var experiment=module.exports.load_experiment()
+    var vi_used = lista 
+    var vi_usado =['VI.vi',experiment.phases[index_phase].componentA_vi]
+    vi_usado=vi_usado.join('')
+        vi_usado += '.tamanho'
+        vi_usado = vi_usado.split('.')
+    vi_usado.forEach((item) => {
+      vi_used = vi_used[item]
+    })
+    return vi_used
+  },
+  load_experiment:function(){//carrega experiment.json
+    var fs = require('fs'),
+    path = require('path')
+    filePath = path.join(__dirname+'/json', 'experiment.json');
+    try {
+        var fsexperiment = fs.readFileSync(filePath)
+        var experiment = JSON.parse(fsexperiment);
+    } catch (error) {
         return false;
-      }
-      var max =archive.VI.vi20.tamanho;
-      console.log("max:",max);
-      var i;
-      var list= [];
-      for(i=0;i<max;i++){
-          list[i]=archive.VI.vi20.vi[i];
-      }
-      return list;
+    }
+    return experiment
+  },
+  return_lista:function(){//carrega lista.json
+    var fs = require('fs'),
+    path = require('path')
+    filePath = path.join(__dirname+'/json', 'lista.json');
+    try {
+        var fslista = fs.readFileSync(filePath)
+        var lista = JSON.parse(fslista);
+    } catch (error) {
+        return false;
+    }
+    return lista
+  },
+  define_vi_cpA : function(fase){ //devolve o vi usado no teste no componete 1 //func em teste
+    var experiment=module.exports.load_experiment();
+    number_of_phases=experiment.number_of_phases
+    return experiment.phases[fase].componentA_vi
   },
   embaralha : function(vi){//embaralha as variaveis
     var index_atual = vi.length, temp, index_aleatorio;
@@ -57,7 +103,7 @@ module.exports = {
     }
     return vi;
   },
-  remove_da_lista :function(vi, returns){//remove o item usado da lista    
+  remove_da_lista :function(vi, returns){//remove o item usado da lista
     var i;
     var j=0;
     var temp=[];
@@ -69,6 +115,48 @@ module.exports = {
       }
     }
     return temp;
+  },
+  /*componenteB */
+  gera_vi_componentB: function(){//está função devolve um valor do vi especifico
+
+    if(vi_componentB[0]===undefined){
+      vi_componentB=module.exports.carries_lista_componentB();//isso talvez não deve ficar em definitivo aqui
+      vi_componentB=module.exports.embaralha(vi_componentB);//embaralha aqui
+    }
+    interval_componentB=module.exports.gera_intervalo(vi_componentB,interval_componentB);//talvez tenha problema
+    vi_componentB=module.exports.remove_da_lista(vi_componentB,interval_componentB);
+    return interval_componentB;
+  },
+  carries_lista_componentB : function(){//carrega o json para as variaveis
+    var lista=module.exports.return_lista();
+    var vi_componentB=module.exports.define_vi_cpA(index_phase);
+    var vi_used=module.exports.load_vi_componentB(lista)//vi_used//_componentB
+    var max=module.exports.max_of_vi(vi_used,lista)
+    return module.exports.interval_list(lista,vi_used);
+  },
+  load_vi_componentB: function(lista){//func em teste
+    var experiment=module.exports.load_experiment()
+    var vi_used = lista 
+
+    var vi_usado =['VI.vi',experiment.phases[index_phase].componentB_vi]
+    vi_usado=vi_usado.join('')
+        vi_usado += '.tamanho'
+        vi_usado = vi_usado.split('.')
+        //aqui
+    vi_usado.forEach((item) => {
+      vi_used = vi_used[item]
+    })
+    return vi_used
+  },
+  define_vi_cpA : function(fase){ //devolve o vi usado no teste no componete 1 //func em teste
+    var experiment=module.exports.load_experiment();
+    number_of_phases=experiment.number_of_phases
+    return experiment.phases[fase].componentB_vi
+  },
+  change_phase: function(){
+    if (index_phase<number_of_phases){
+      index_phase+=1;
+    }
   }
 };
 
--- app/gerarelatorio.js
+++ app/gerarelatorio.js
@@ -1,29 +1,140 @@
-/* 
-    distinguir se o componente possui vi constante ou alternado
-    VIs usado no teste
-    numero de respostas pra cada componete(numeros de cliques/tempo de sessao em minutos)
- */
-
-
+/*criar variaveis usados na estrutura json ressultados*/
+var report_results=[];
+var phases=[];
+var component_A;
+var component_B;
 module.exports = {
 
-    gera_relatorio: function(){
-        const fs = require('fs')
-        const PDFDocument = require('pdfkit')//chama as funcoes do modulo pdfkit
-
-        const doc = new PDFDocument;
-
-        doc.pipe(fs.createWriteStream('./PDF/test.pdf'));// aqui troca o local aonde o aquivo sera salvo //createWriteStream - cria um arquivo editavel no local especificado
-
-        var jsonText = [
-            {'1': 'novo'},
-            {'2': 'novo2'}
-        ]
-
-        jsonText.forEach(function(item){
-            doc.text(JSON.stringify(item));
+    create_report:function(componentA,componentB){
+        /*carrega os dados necessarios*/
+        experiment=module.exports.load_experiment();//experiment.json
+        lista=module.exports.load_lista();//lista.json
+        /* escreve nas variaveis o necessario para o json resultado*/
+        var session_time=experiment.game_time //tempo de sessão
+        var training_time=experiment.training_time//tempo de treino
+        var number_of_phases=experiment.number_of_phases //numero de fases
+        var show_score_interval=experiment.score_show //tempo de espera para mostrar a pontuação 
+        var total_click_number=componentA.click+componentB.click //calculo feito (cliques no componete A + cliques no componete B + cliques fora do componete A + cliques fora do componete B)
+        var component_time=experiment.component_time //tempo de exibição de cada componente
+        //numero de fases e fazer o processo abaixo pra cada
+        
+        /*array phases*/
+        for(i=0;i<number_of_phases;i++){
+            if(i>0 && i<number_of_phases){
+                phases+=", "
+            }
+            var number_of_cycles=experiment.phases[i].number_of_cycles // quantidade de ciclos de aparição de cada componente
+            var interval_time=experiment.phases[i].interval_time // intervalo entre componetes
+            /*Objeto Componente*/
+            component_A=module.exports.build_component(componentA,i,experiment,lista,1)
+            component_B=module.exports.build_component(componentB,i,experiment,lista,2)
+        
+            phases.push({
+            "number_of_cycles":""+number_of_cycles,
+            "interval_time":""+interval_time,
+            component_A,
+            component_B
+            })
+        }
+        report_results= {
+            "session_time":session_time,
+            "training_time":training_time,
+            "show_score_interval": show_score_interval,
+            "total_click_number":""+total_click_number,
+            "component_time":component_time,
+            "number of phases":number_of_phases,
+            "phases":phases
+        }
+        module.exports.whrite_report(report_results)
+    },
+    load_experiment:function(){//carrega experiment.json
+        var fs = require('fs'),
+        path = require('path')
+        filePath = path.join(__dirname+'/json', 'experiment.json');
+        try {
+            var fsexperiment = fs.readFileSync(filePath)
+            var experiment = JSON.parse(fsexperiment);
+        } catch (error) {
+            return false;
+        }
+        return experiment
+    },
+    //carrega lista.json
+    load_lista:function(){
+        var fs = require('fs'),
+        path = require('path')
+        filePath = path.join(__dirname+'/json', 'lista.json');
+        try {
+            var fslista = fs.readFileSync(filePath)
+            var lista = JSON.parse(fslista);
+        } catch (error) {
+            return false;
+        }
+        return lista
+    },
+    
+    //constroi o componente
+    build_component:function(temp_component,index_phase,experiment,lista,ab){
+    
+        if(ab==1){
+            var vi_used=experiment.phases[index_phase].componentA_vi //VI usado no teste para o componente A
+        }
+        else{
+            var vi_used=experiment.phases[index_phase].componentB_vi //VI usado no teste para o esse componente B
+        }
+    
+        var vi_usado =['VI.vi',experiment.phases[index_phase].componentA_vi]
+    
+        if(ab==1){
+            var number_of_clicks=componentA.click //carrega do objeto que veio de teste_sequencial
+        }
+        else{
+            var number_of_clicks=componentB.click //carrega do objeto que veio de teste_sequencial
+        }
+    
+        vi_usado=vi_usado.join('')
+        vi_usado += '.tamanho'
+        vi_usado = vi_usado.split('.')
+        
+        var number_of_intevals = lista //numero de intervalos usado no componente
+        
+        vi_usado.forEach((item) => {
+            number_of_intevals = number_of_intevals[item]
         })
-
-        doc.end()
+        
+        if(ab==1){
+            var color=experiment.phases[index_phase].componentA_color //cor do componente      
+        }
+        else{
+            var color=experiment.phases[index_phase].componentB_color //cor do componente      
+        }
+    
+        return temp_component={
+            "vi_used":"vi"+vi_used,
+            "number_of_clicks":""+number_of_clicks,
+            "number_of_intevals":""+number_of_intevals,
+            "color":color
+        }
+    },
+    /*grava relatorio*/
+    whrite_report :function(report_results){
+        var archive;
+        var fs = require('fs'),
+        path = require('path')
+        filePath = path.join(__dirname+'/json', 'results.json' );
+        try {
+            var fsarchive = fs.readFileSync(filePath)
+            var archive = JSON.parse(fsarchive);
+        } catch (error) {
+            console.log("error")
+            return false;
+        }
+    
+        archive.push(report_results)
+        let data = JSON.stringify(archive);
+        console.log(archive); 
+    
+        fs.writeFileSync(__dirname+'/json/results.json', data);
+    
     }
 };
