8838a6657afbfa2c55db033d9bbc19af224e1d71
vitasso13 <43220494+vitasso13@users.noreply.github.com>
2019-11-04 09:38:21 -0400

    v1.0
    Após refatora

---
+++ app/main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  // Cria uma janela de navegação.
+  let win = new BrowserWindow({
+    width: 1600,
+    height: 900,
+    resizable: true,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  // e carregar o index.html do aplicativo.
+  win.loadFile("userIndex.html");
+}
+
+app.on("ready", createWindow);
+
