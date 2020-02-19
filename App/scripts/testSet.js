237ebb9362edcb7e17390f6daad020bd8b6677d7
Victor Augusto Fonseca Pinto <vitao.afp@gmail.com>
2020-02-19 16:36:51 -0400

    Troca de fases e finalização do teste implementada

--- App/scripts/testSet.js
+++ App/scripts/testSet.js
@@ -5,16 +5,21 @@ const modalClass = require("./App/scripts/modal.js");
 const scoreClass = require("./App/scripts/placar.js");
 const VI = require("./App/scripts/gera_vi.js");
 const test = require("./App/scripts/test.js");
-const remote = require('electron').remote;
-const compParamA = expParam.Stages[0].CompA;
-const compParamB = expParam.Stages[0].CompB;
+const remote = require("electron").remote;
+var stageNumber = 0;
+const compParamA = expParam.Stages[stageNumber].CompA;
+const compParamB = expParam.Stages[stageNumber].CompB;
+const report = require("./App/scripts/gerarelatorio.js")
 
 var posVIA = new VI();
 var negVIA = new VI();
 var posVIB = new VI();
 var negVIB = new VI();
 
-var teste = new test(expParam.Stages[0].IntervalTime, expParam.TestSet.TestTime);
+var teste = new test(
+  expParam.Stages[0].IntervalTime,
+  expParam.TestSet.TestTime
+);
 
 var score = new scoreClass(expParam.TestSet.ShowScoreInterval);
 var pointsA = new modalClass(
@@ -51,20 +56,28 @@ var componentB = new componentClass(
 componentA.getClick();
 componentB.getClick();
 pointsA.btn.onclick = function() {
-  teste.beginChanger();  
+  teste.beginChanger();
   componentA.score++;
-  document.getElementById("scoreText").innerHTML = "Pontuação: " + (componentA.score + componentB.score);
+  document.getElementById("scoreText").innerHTML =
+    "Pontuação: " + (componentA.score + componentB.score);
   componentA.posViCount();
-  console.log("Você coletou pontos! Pontos totals: " + (componentA.score + componentB.score));
+  console.log(
+    "Você coletou pontos! Pontos totals: " +
+      (componentA.score + componentB.score)
+  );
   document.getElementById("myModalA").style.display = "none";
 };
 pointsB.btn.onclick = function() {
   teste.beginChanger();
   componentB.score++;
   score.info = "Pontuação: " + componentA.score + componentB.score;
-  document.getElementById("scoreText").innerHTML = "Pontuação: " + (componentA.score + componentB.score);
+  document.getElementById("scoreText").innerHTML =
+    "Pontuação: " + (componentA.score + componentB.score);
   componentB.posViCount();
-  console.log("Você coletou pontos! Pontos totals: " + (componentA.score + componentB.score));
+  console.log(
+    "Você coletou pontos! Pontos totals: " +
+      (componentA.score + componentB.score)
+  );
   document.getElementById("myModalB").style.display = "none";
 };
 teste.beginChanger();
@@ -74,9 +87,15 @@ componentA.posViCount();
 componentB.posViCount();
 
 
-setTimeout(function(){
-  var window = remote.getCurrentWindow();
-  window.close();
-}, teste.phaseTime);
-
-
+while (stageNumber < expParam.StagesSet.NumberOfStages) {
+  stageNumber++;
+  setTimeout(function() {
+    var window = remote.getCurrentWindow();
+        if(stageNumber >= expParam.StagesSet.NumberOfStages){
+          //report.create_report(componentA, componentB)  Não está funcionando
+          window.close();
+        }
+        else
+          window.reload();
+  }, 5000); //teste.phaseTime);
+}
