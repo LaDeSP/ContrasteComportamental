b2b1562127646c2616cacef6fdc3058ad14bac4a
Victor Augusto Fonseca Pinto <vitao.afp@gmail.com>
2020-02-20 15:13:25 -0400

    Teste alternado com geração de relatório

--- App/scripts/testSet.js
+++ App/scripts/testSet.js
@@ -5,22 +5,27 @@ const modalClass = require("./App/scripts/modal.js");
 const scoreClass = require("./App/scripts/placar.js");
 const VI = require("./App/scripts/gera_vi.js");
 const test = require("./App/scripts/test.js");
-const report = require("./App/scripts/genarateReport.js");//teste
-const itControler = require("./App/scripts/itControler.js");//teste
-const remote = require('electron').remote;
-const maxIndex = expParam.StagesSet.NumberOfStages;//teste
-var index = 0;//teste
+const report = require("./App/scripts/genarateReport.js"); //teste
+const itControler = require("./App/scripts/itControler.js"); //teste
+const remote = require("electron").remote;
+const maxIndex = expParam.StagesSet.NumberOfStages; //teste
+var index = 0; //teste
 var compParamA = expParam.Stages[index].CompA;
 var compParamB = expParam.Stages[index].CompB;
-var compPAIT= new itControler();//teste
-var compPBIT= new itControler();//teste
+var compPAIT = new itControler(); //teste
+var compPBIT = new itControler(); //teste
 
 var posVIA = new VI();
 var negVIA = new VI();
 var posVIB = new VI();
 var negVIB = new VI();
 
-var teste = new test(expParam.Stages[index].IntervalTime, expParam.TestSet.TestTime, compPAIT, compPBIT);
+var teste = new test(
+  expParam.Stages[index].IntervalTime,
+  expParam.TestSet.TestTime,
+  compPAIT,
+  compPBIT
+);
 
 var score = new scoreClass(expParam.TestSet.ShowScoreInterval);
 var pointsA = new modalClass(
@@ -57,7 +62,7 @@ var componentB = new componentClass(
   posVIB,
   negVIB,
   pointsB,
-  "capiB",  
+  "capiB",
   compParamB.PosPtsGive,
   compParamB.NegPtsGive,
   compParamB.ComponentViPOS,
@@ -67,20 +72,28 @@ var componentB = new componentClass(
 componentA.getClick();
 componentB.getClick();
 pointsA.btn.onclick = function() {
-  teste.beginChanger();  
+  teste.beginChanger();
   componentA.score++;
-  document.getElementById("scoreText").innerHTML = "Pontuação: " + (componentA.score + componentB.score);
+  document.getElementById("scoreText").innerHTML =
+    "Pontuação: " + (componentA.score + componentB.score);
   componentA.posViCount();
-  console.log("Você coletou pontos! Total de pontos: " + (componentA.score + componentB.score));
+  console.log(
+    "Você coletou pontos! Total de pontos: " +
+      (componentA.score + componentB.score)
+  );
   document.getElementById("myModalA").style.display = "none";
 };
 pointsB.btn.onclick = function() {
   teste.beginChanger();
   componentB.score++;
   score.info = "Pontuação: " + componentA.score + componentB.score;
-  document.getElementById("scoreText").innerHTML = "Pontuação: " + (componentA.score + componentB.score);
+  document.getElementById("scoreText").innerHTML =
+    "Pontuação: " + (componentA.score + componentB.score);
   componentB.posViCount();
-  console.log("Você coletou pontos! Pontos totals: " + (componentA.score + componentB.score));
+  console.log(
+    "Você coletou pontos! Pontos totals: " +
+      (componentA.score + componentB.score)
+  );
   document.getElementById("myModalB").style.display = "none";
 };
 teste.beginChanger();
@@ -89,18 +102,16 @@ score.scoreShow(score.showInterval, "pos", componentA, teste.compChange);
 componentA.posViCount();
 componentB.posViCount();
 
-report.storageComps(componentA,componentB);
 while (index < expParam.StagesSet.NumberOfStages) {
   index++;
   setTimeout(function() {
     var window = remote.getCurrentWindow();
-        if(index >= expParam.StagesSet.NumberOfStages){
-          report.makeReport(expParam, componentA, componentB)
-          window.close();
-        }
-        else
-          window.reload();
-  }, teste.phaseTime);
+    if (index >= expParam.StagesSet.NumberOfStages) {
+      report.makeReport(expParam, componentA, componentB);
+      window.close();
+    } else {
+      report.storageComps(componentA, componentB);
+      window.reload();
+    }
+  }, 20000); //teste.phaseTime);
 }
-
-
